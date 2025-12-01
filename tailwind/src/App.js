6f3bd1f22257db9965da91dd1477c79457e79013
dilani2002-dila <dilani.uki.stu@dreamspace.academy>
2025-12-01 15:15:40 +0530

    created files

---
+++ tailwind/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Tailwind from './components/tailwind';
+
+function App() {
+  return (
+  
+    <div className='app'>
+    <Tailwind></Tailwind>
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind/src/components/tailwind.jsx
@@ -0,0 +1,10 @@
+export default function Tailwind(){
+    return(
+        <h1 style={{fontSize:"80px",color:"green", textAlign:"center"}} className="text-3xl font-bold underline">Hello World Dil!!</h1>
+    );
+}
+
+
+
+
+
